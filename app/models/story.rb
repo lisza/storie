f0b0799c803b25c1f6b7bf65f7b987dc63cb83f5
lisza <liszalisza@gmail.com>
2017-11-03 17:01:29 -0700

    Change paperclip defaults such that StoriesIndexThumbs will not default to an image if none was supplied

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,10 +1,8 @@
 class Story < ApplicationRecord
   validates :title, :body, :author_id, presence: true
   # Paperclip defaults:
-  has_attached_file :image, default_url: "default.jpg"
+  has_attached_file :image, default_url: ""
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
-  # before_save :set_default_image
 
   belongs_to :author,
     primary_key: :id,
@@ -12,8 +10,4 @@ class Story < ApplicationRecord
     class_name: :User
 
   has_many :comments, dependent: :destroy
-
-  # def set_default_image
-  #   self.image_url ||= "http://68.media.tumblr.com/47f1bb26b315fa4c053d4ce12c6a9233/tumblr_mq56lpv7oh1r6w3qso1_500.jpg"
-  # end
 end
