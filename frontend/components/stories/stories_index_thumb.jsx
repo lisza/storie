b49b37a6d0160e4fe96ae05fe10c28933ac20140
lisza <liszalisza@gmail.com>
2017-11-13 16:59:51 -0800

    Add dynamic sizing to indexThumb components

--- frontend/components/stories/stories_index_thumb.jsx
+++ frontend/components/stories/stories_index_thumb.jsx
@@ -4,6 +4,7 @@ import ShowStoryContainer from './show_story_container';
 import UserThumb from '../users/user_thumb';
 
 const StoryIndexThumb = ({ story }) => {
+  // Manipulates flex-basis style such that boxes have varible sizes
   const thumbSize = () => {
     const sizes = [20, 30, 30, 30, 60];
     return sizes[Math.floor(Math.random() * sizes.length)];
