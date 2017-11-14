ae99a0a49d74f411b0d8f6dfd4027e91e8193f0f
lisza <liszalisza@gmail.com>
2017-11-13 16:54:38 -0800

    Add randomized box sizing to indexThumb components

--- frontend/components/stories/stories_index_thumb.jsx
+++ frontend/components/stories/stories_index_thumb.jsx
@@ -3,30 +3,39 @@ import { Link } from 'react-router-dom';
 import ShowStoryContainer from './show_story_container';
 import UserThumb from '../users/user_thumb';
 
-const StoryIndexThumb = ({ story }) => (
+const StoryIndexThumb = ({ story }) => {
+  const thumbSize = () => {
+    const sizes = [20, 30, 30, 30, 60];
+    return sizes[Math.floor(Math.random() * sizes.length)];
+  };
 
-  <div className="story-thumb">
-      <Link to={`/stories/${story.id}`}>
+  const style = {
+    flexBasis: `${thumbSize()}` + '%'
+  };
 
-        { story.image_url === "" ? null :
-          <section className="story-thumb-image">
-            <img src={story.image_url} />
+  return (
+    <div className="story-thumb" style={style}>
+        <Link to={`/stories/${story.id}`}>
+
+          { story.image_url === "" ? null :
+            <section className="story-thumb-image">
+              <img src={story.image_url} />
+            </section>
+          }
+
+          <section className="story-thumb-content">
+            <h1>{story.title}</h1>
+            {story.description ? <h3>{story.description}</h3>
+            : <p>{story.body.slice(0,100)}...</p>}
           </section>
-        }
+        </Link>
 
-        <section className="story-thumb-content">
-          <h1>{story.title}</h1>
-          {story.description ? <h3>{story.description}</h3>
-          : <p>{story.body.slice(0,100)}...</p>}
-        </section>
-      </Link>
-
-      <UserThumb
-        userId={story.author.author_id}
-        username={story.author.author_name}
-        userImage={story.author.author_image}
-        postDate={story.created_at} />
-  </div>
-);
+        <UserThumb
+          userId={story.author.author_id}
+          username={story.author.author_name}
+          userImage={story.author.author_image}
+          postDate={story.created_at} />
+    </div>
+)};
 
 export default StoryIndexThumb;
